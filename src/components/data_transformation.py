8953946ec2370c8f7255c24651bf38cdfb43022c
saikiran2508 <gsaikiran502@gmail.com>
2024-08-10 16:15:18 -0400

    data transformation done

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -15,7 +15,7 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path = os.path.join('artifact',"preprocessor.pkl")
+    preprocessor_obj_file_path = os.path.join('artifacts',"preprocessor.pkl")
     
 
 class DataTransformation:
